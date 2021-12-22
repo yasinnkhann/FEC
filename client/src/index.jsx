b26ec0dba84da643ef123b785b14e2055978e151
Kenneth Bakke <kennethsbakke@gmail.com>
2021-12-21 16:01:37 -0800

    Add module.hot reload check in index.js

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,5 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
+
+if (module.hot) {
+  module.hot.accept();
+}
+
 ReactDOM.render(<App />, document.getElementById('app'));
 
