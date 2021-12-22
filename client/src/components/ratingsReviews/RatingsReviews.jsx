859607995dd7148d4fa8855418011af85d3e39ee
Hatha Dam <hathadam9@gmail.com>
2021-12-22 12:10:16 -0800

    update ratingReviews

--- client/src/components/ratingsReviews/RatingsReviews.jsx
+++ client/src/components/ratingsReviews/RatingsReviews.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+// import { TOKEN } from '../config.js';
 
 export default function RatingsReviews() {
+  //TODO: Will focus on this part
+  // useEffect(() => {
+  // const getApi = async () => {
+  //   try {
+  //     const res = await axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products', {
+  //       headers: {
+  //         'Authorization': `${TOKEN}`
+  //       }
+  //     });
+  //     console.log(res.data);
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
+  //   getApi();
+  // }, []);
   return (
     <div>
-
       <h1>Hello from ratings and reviews!</h1>
       <h1>This is Hatha's part Testing git flow!</h1>
 
     </div>
   );
 }
+
