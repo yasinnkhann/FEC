6220e5f21d9a7737af0824f5a1cf23a2bc093cfa
Yasin Khan <yasinkhan500@gmail.com>
2021-12-22 21:45:49 -0800

    stache

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
-// var path = require('path');
-// var SRC_DIR = path.join(__dirname, '/client/src');
-// var DIST_DIR = path.join(__dirname, '/client/dist');
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
 
-// module.exports = {
-//   entry: `${SRC_DIR}/index.jsx`,
-//   output: {
-//     filename: 'bundle.js',
-//     path: DIST_DIR
-//   },
-//   devtool: 'eval-source-map',
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(js|jsx)?/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: [
-//               '@babel/preset-env',
-//               '@babel/preset-react'
-//             ],
-//             plugins: [
-//               ['@babel/plugin-transform-runtime',
-//                 {
-//                   'regenerator': true
-//                 }
-//               ]
-//             ]
-//           }
-//         }
-//       }
-//     ]
-//   }
-// };
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  devtool: 'eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ],
+            plugins: [
+              ['@babel/plugin-transform-runtime',
+                {
+                  'regenerator': true
+                }
+              ]
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
