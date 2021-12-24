09d101f8334cd480c20d399922c31abb08e44cac
Hatha Dam <hathadam9@gmail.com>
2021-12-23 17:16:38 -0800

    adding dummy data to rating reviews and in progress for rating reviews

--- client/src/components/ratingsReviews/RatingsReviews.jsx
+++ client/src/components/ratingsReviews/RatingsReviews.jsx
@@ -1,33 +1,56 @@
+/* eslint-disable camelcase */
 import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import { TOKEN } from '../../config.js';
 import AppContext from '../../AppContext.js';
+import Reviews from './Reviews.jsx';
+
+// var dataList = ['Kale', 'Cucumbers', 'Hot Pot', 'Ginger'];
+const ReviewList = (props) => {
+
+};
 
 export default function RatingsReviews() {
   const { products, setProducts } = useContext(AppContext);
+  const [ratingAndReviews, setRatingAndReviews] = useState([]);
 
-  //TODO: Will focus on this part
-  // useEffect(() => {
-  // const getApi = async () => {
-  //   try {
-  //     const res = await axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products', {
-  //       headers: {
-  //         'Authorization': `${TOKEN}`
-  //       }
-  //     });
-  //     console.log(res.data);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
-  //   getApi();
-  // }, []);
+  useEffect(() => {
+    const getApi = async () => {
+      try {
+        const res = await axios.get(
+          'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/',
+          {
+            params: {
+              // page: 1,
+              count: 50,
+              // sort: 'newest',
+              product_id: 40344
+            },
+            headers: {
+              Authorization: `${TOKEN}`,
+            }
+          }
+        );
+        // console.log('Rate and Review API Data:', res.data);
+        setRatingAndReviews(res.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getApi();
+  }, []);
 
   return (
     <div>
       <p>Hello from ratings and reviews!!TESTING!!</p>
-      {console.log('from Rating and Reviews', products)}
+      {/* {console.log('from Product from APP:', products[0]?.id)} */}
+      {/* {console.log('from RatingAndReviews:', ratingAndReviews)} */}
+      <div>
+        {/* <Reviews data={this.state.ratingAndReviews}/> */}
+      </div>
     </div>
   );
 }
+
 
