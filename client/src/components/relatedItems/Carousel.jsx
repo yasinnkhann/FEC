2f23317f5b2baf7f4a22d27d426b877077e0e5d5
Kenneth Bakke <kennethsbakke@gmail.com>
2021-12-23 16:57:43 -0800

    Build out carousel skeleton, set up card component and smaller components for various cards

--- client/src/components/relatedItems/Carousel.jsx
+++ client/src/components/relatedItems/Carousel.jsx
@@ -1,19 +1,27 @@
 import React, {useContext} from 'react';
 import AppContext from '../../AppContext.js';
+import ScrollArrows from './ScrollArrows.jsx';
+import Card from './Card.jsx';
+
+/**
+ * WILL BE THE OUTER DIV FOR BOTH LISTS: RELATED PRODUCTS AND YOUR OUTFIT
+ */
 
 export default function Carousel() {
+  const {products, setProducts} = useContext(AppContext);
+  const productsShown = products.slice(0, 10);
   return (
-    <div className="carousel">
+    <div className="carousel" >
       This is the carousel
-      <div className="carousel-row">
-        <div className="carousel-left">
-          This is carousel left
+      <div className="carousel-row" style={{display: 'flex'}} >
+        <div className="carousel-left" onClick={()=> console.log('Left click')} >
+          <ScrollArrows.LeftArrow />
         </div>
-        <div className="carousel-middle">
-          This is carousel middle
+        <div className="carousel-middle" style={{display: 'flex'}} >
+          {productsShown.map(product => <Card key={product.id} product={product} />)}
         </div>
-        <div className="carousel-right">
-          This is carousel right
+        <div className="carousel-right" onClick={()=> console.log('Right click')} >
+          <ScrollArrows.RightArrow />
         </div>
       </div>
     </div>
--- client/src/components/relatedItems/RelatedItems.jsx
+++ client/src/components/relatedItems/RelatedItems.jsx
@@ -8,33 +8,36 @@ const URL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/';
 
 export default function RelatedItems() {
   const { products, setProducts } = useContext(AppContext);
+  const [qa, setQa] = useState([]);
 
   useEffect(() => {
     const getQa = async () => {
       try {
-        const questionsAndAnswers = await axios.get(
-          `${URL}products`,
+        const res = await axios.get(
+          `${URL}reviews/`,
           {
             params: {
-              // page: 1,
-              count: 20,
-              // sort: 'newest',
-              // product_id: null
+              page: 1,
+              count: 10,
+              sort: 'newest',
+              product_id: 40344
             },
             headers: {
               Authorization: `${TOKEN}`,
             }
           }
         );
-        return questionsAndAnswers;
+        setQa(res.data);
       } catch (err) {
         console.error(err);
       }
     };
+
+    getQa();
   }, []);
 
   return (
-    <div className="related-items-and-comparison">
+    <div className="related-items-and-comparison" >
       This is the Related Items Section
       <Carousel />
     </div>
