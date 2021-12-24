9c746a1dca09c1e2455b1252968117119e9c901a
Kenneth Bakke <kennethsbakke@gmail.com>
2021-12-23 16:58:28 -0800

    Stash

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,35 +7,25 @@ import RelatedItems from './relatedItems/RelatedItems.jsx';
 import { TOKEN } from '../config.js';
 import AppContext from '../AppContext.js';
 
-<<<<<<< HEAD
-const App = () => {
-=======
 export default function App() {
   const [products, setProducts] = useState([]);
->>>>>>> 6885d44abec81a83745f8477c6a4a82d07ab306f
 
   useEffect(() => {
     const getApi = async () => {
       try {
-<<<<<<< HEAD
-        const res = await axios.get('http://localhost:3000/api', {
-          headers: {
-            'Authorization': `${TOKEN}`
-=======
         const res = await axios.get(
           'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products',
           {
             params: {
               // page: 1,
-              count: 2000,
+              count: 20,
             },
             headers: {
               Authorization: `${TOKEN}`,
             },
->>>>>>> 6885d44abec81a83745f8477c6a4a82d07ab306f
           }
         );
-        console.log(res.data);
+        // console.log(res.data);
         setProducts(res.data);
       } catch (err) {
         console.error(err);
@@ -56,5 +46,3 @@ export default function App() {
     </div>
   );
 }
-
-export default App;
