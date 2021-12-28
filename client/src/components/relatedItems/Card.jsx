48a6a51599cb18d49cb46a02d287a34d208752b4
Kenneth Bakke <kennethsbakke@gmail.com>
2021-12-28 11:55:08 -0800

    Add selectedProduct state to App.js, refactor AppContext.Provider to send both products array and selectedProduct item as context. Add example comment in RelatedItems.jsx for proper usage of getting state needed from context. Also minor adds and changes to relatedItems directory

--- client/src/components/relatedItems/Card.jsx
+++ client/src/components/relatedItems/Card.jsx
@@ -1,11 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import AppContext from '../../AppContext.js';
 import ProductInfo from './ProductInfo.jsx';
 
 export default function CarouselCard({ product }) {
 
+  // CONTEXT
+  const {selectedProduct, setSelectedProduct} = useContext(AppContext);
+
+  // LOGS
+  // console.log('PRODUCT::', product);
+  // console.log('CONTEXT SELECTEDPRODUCT::', selectedProduct);
+  // console.log('STATE NEWSELECTEDPRODUCT::', newSelectedProduct);
+
   return (
-    <div className="carousel-card" style={{border: '1px solid black'}} >
+    <div className="carousel-card" style={{border: '1px solid black'}} onClick={() => setSelectedProduct(product)} >
       <img alt="product image" />
       <ProductInfo product={product} />
     </div>
--- client/src/components/relatedItems/RelatedItems.jsx
+++ client/src/components/relatedItems/RelatedItems.jsx
@@ -9,39 +9,51 @@ const URL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/';
 
 
 export default function RelatedItems() {
-  const { products, setProducts } = useContext(AppContext);
-  const [qa, setQa] = useState([]);
+
+  // OLD METHOD -> destructure the products array and the setProducts method for use
+  // const {products, setProducts} = useContext(AppContext)
+
+  // NEW METHOD -> destructure the state you want to use (line 18) then from that state get your products array and the setProduct method (line 19)
+  // const {productsValue} = useContext(AppContext)
+  // const [products, setProducts] = productsValue
+
+  // CONTEXT
+  const {selectedProductValue} = useContext(AppContext)
+  const [selectedProduct, setSelectedProduct] = selectedProductValue
+
+  // STATE
+  const [relatedProducts, setRelatedProducts] = useState([])
 
   useEffect(() => {
-    const getQa = async () => {
+    const getRelatedProducts = async () => {
       try {
         const res = await axios.get(
-          `${URL}reviews/`,
+          `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp//products/:${setSelectedProduct.id}/related`,
           {
             params: {
-              page: 1,
-              count: 10,
-              sort: 'newest',
-              product_id: 40344
+              // page: 1,
+              count: 2000,
             },
             headers: {
               Authorization: `${TOKEN}`,
-            }
+            },
           }
         );
-        setQa(res.data);
+        setRelatedProducts(res.data);
       } catch (err) {
         console.error(err);
       }
     };
 
-    getQa();
-  }, []);
+    getRelatedProducts();
+  }, [])
 
   return (
     <div className="related-items-and-comparison" >
-      This is the Related Items Section
-      <Carousel />
+      <h3>RELATED ITEMS</h3>
+      <Carousel name="related-items"/>
+      <h3>YOUR OUTFIT</h3>
+      <Carousel name="your-outfit" />
     </div>
   );
 }
