96918495f5c678842354f1f6724d2811a60b9714
Kenneth Bakke <kennethsbakke@gmail.com>
2021-12-30 10:44:33 -0800

    Stash

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -52,7 +52,7 @@ export default function App() {
             <Overview />
             <RelatedItems />
             <QuestionsAnswers />
-            <RatingsReviews />
+            {/* <RatingsReviews /> */}
           </AppContext.Provider>
         </>
       ) : (
