4bea073c339b53988fab15e5c1d21f24e44d3800
Ellie Hunt <elliehunt8@gmail.com>
2022-01-04 10:27:14 -0700

    Update StarFilterEntry.jsx

--- client/src/components/ratingsReviews/ratingBreakdown/StarFilterEntry.jsx
+++ client/src/components/ratingsReviews/ratingBreakdown/StarFilterEntry.jsx
@@ -2,32 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const starStyle = {
-<<<<<<< HEAD
-  // display: 'flex',
-  // marginLeft: '2px',
-  // marginRight: '2px',
-  // width: '30px',
-  // textAlign: 'center',
-  // borderRadius: '40px',
-  // padding: '5px',
-  // fontSize: '10px',
-  // border: 'none',
-  // boxShadow: '2px 2px 4px gold',
-  // cursor: 'pointer',
   display: 'flex', marginLeft: '2px', marginRight: '2px', width: '30px', textAlign: 'center', borderRadius: '40px', padding: '5px', fontSize: '10px', border: 'none', boxShadow: '2px 2px 4px green', cursor: 'pointer',
-=======
-  display: 'flex',
-  marginLeft: '2px',
-  marginRight: '2px',
-  width: '30px',
-  textAlign: 'center',
-  borderRadius: '40px',
-  padding: '5px',
-  fontSize: '10px',
-  border: 'none',
-  boxShadow: '2px 2px 4px gold',
-  cursor: 'pointer'
->>>>>>> 2dc96cb2d27277eb705625408f2d73efb0d60541
 };
 
 const StarFilterEntry = (props) => (
