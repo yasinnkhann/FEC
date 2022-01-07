d37b84e3b2ca6af0c6f9f5b44eccadd7024609af
Kenneth Bakke <kennethsbakke@gmail.com>
2022-01-06 17:50:15 -0800

    Change port to 80

--- client/src/components/relatedItems/Card.jsx
+++ client/src/components/relatedItems/Card.jsx
@@ -14,7 +14,7 @@ import Modal from './Modal.jsx';
 import ProductPreviewImages from './ProductPreviewImages.jsx';
 import ProductInfo from './ProductInfo.jsx';
 
-const URL = 'http://34.223.4.224:3000/api';
+const URL = 'http://34.223.4.224:80/api';
 
 // CARD
 export default function CarouselCard({ product, name }) {
