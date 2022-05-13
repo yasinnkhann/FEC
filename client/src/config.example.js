7b66b9209da1c4dbea437a250f6bb1009dc6d918
Yasin Khan <yasinkhan500@gmail.com>
2022-05-12 22:00:13 -0700

    stache

---
+++ client/src/config.example.js
@@ -0,0 +1,11 @@
+export const serverURL = '';
+
+export const cloudinaryInfo = {
+  CLOUDINARY_NAME: '',
+  CLOUDINARY_API_KEY: '',
+  CLOUDINARY_API_SECRET: '',
+  CLOUDINARY_IMAGE_UPLOAD_URL: '',
+  CLOUDINARY_API_BASE_URL: '',
+  CLOUDINARY_SECURE_DELIVERY_URL: '',
+  CLOUDINARY_UPLOAD_PRESET: '',
+};
